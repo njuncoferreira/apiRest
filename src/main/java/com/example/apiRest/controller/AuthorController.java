9737db12777e09a4009b482a4e0e7771cf98cc0c
Nathan Ferreira <c0659465@vale.com>
2025-06-16 12:25:38 -0300

    Complete Author Controller

--- src/main/java/com/example/apiRest/controller/AuthorController.java
+++ src/main/java/com/example/apiRest/controller/AuthorController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -23,24 +25,22 @@ public class AuthorController {
 	public AuthorController(AuthorService service) {
 		this.service = service;
 	}
-	
-	@PostMapping
-	public ResponseEntity<Author> save(@RequestBody AuthorDTO authorDTO) {
-		Author authorEntity = authorDTO.AuthorMap();
-		service.save(authorEntity);
-		
-		URI location = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(authorEntity.getId())
-				.toUri();
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.setLocation(location);
-		
-		return new ResponseEntity<Author>(authorEntity, headers, HttpStatus.CREATED);
+
+	@GetMapping
+	public ResponseEntity<List<AuthorDTO>> search(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "nationality", required = false) String nationality){
+		List<Author> result = service.search(name, nationality);
+		List<AuthorDTO> list = result
+				.stream()
+				.map(author -> new AuthorDTO(
+						author.getId(),
+						author.getName(),
+						author.getAnniversaryDate(),
+						author.getNationality())
+				).collect(Collectors.toList());
+
+		return ResponseEntity.ok(list);
 	}
-	
+
 	@GetMapping("{id}")
 	public ResponseEntity<AuthorDTO> getById(@PathVariable("id") String id){
 		var authorId = UUID.fromString(id);
@@ -56,5 +56,54 @@ public class AuthorController {
 		}
 
 		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Author> save(@RequestBody AuthorDTO authorDTO) {
+		Author authorEntity = authorDTO.AuthorMap();
+		service.save(authorEntity);
+
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(authorEntity.getId())
+				.toUri();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(location);
+
+		return new ResponseEntity<Author>(authorEntity, headers, HttpStatus.CREATED);
+	}
+
+	@PutMapping("{id}")
+	public ResponseEntity<Void> update(@PathVariable("id") String id, @RequestBody AuthorDTO dto) {
+		var authorId = UUID.fromString(id);
+		Optional<Author> authorOptional = service.getById(authorId);
+
+		if(authorOptional.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		var author = authorOptional.get();
+		author.setName(dto.name());
+		author.setNationality(dto.nationality());
+		author.setAnniversaryDate(dto.anniversaryDate());
+
+		service.update(author);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> delete(@PathVariable("id") String id) {
+		var authorId = UUID.fromString(id);
+		Optional<Author> authorOptional = service.getById(authorId);
+
+		if(authorOptional.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		service.delete(authorOptional.get());
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/example/apiRest/repository/AuthorRepository.java
+++ src/main/java/com/example/apiRest/repository/AuthorRepository.java
@@ -3,7 +3,13 @@ package com.example.apiRest.repository;
 import com.example.apiRest.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 
-public interface AuthorRepository extends JpaRepository<Author, UUID> {}
+public interface AuthorRepository extends JpaRepository<Author, UUID> {
+
+    List<Author> findByName(String name);
+    List<Author> findByNationality(String nationality);
+    List<Author> findByNameAndNationality(String name, String nationality);
+}
